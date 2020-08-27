e2f94f1e329abb94b8d9f7948a47235646ad9e6a
samga <samgalaxy111111@gmail.com>
2020-08-27 16:58:04 +0700

    TabLayout

--- app/src/main/java/id/sam/mytablayout/MainActivity.java
+++ app/src/main/java/id/sam/mytablayout/MainActivity.java
@@ -1,8 +1,11 @@
 package id.sam.mytablayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        getSupportActionBar().setElevation(0);
     }
 }
